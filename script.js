3c7aa6e6ac26f36e66ab7e58f37f5bdb65b257bc
Cerdtruir <ghoornabeel@gmail.com>
2022-02-20 18:02:29 +0200

    add navbar toggle between x and hsmburger

--- script.js
+++ script.js
@@ -124,3 +124,14 @@ function displaySpeakers() {
   console.log(document.querySelector('.d-md-flex'));
 }
 
+document.querySelector('.navbar-toggler').onclick = function () {
+  hamburger = document.querySelector('.navbar-toggler');
+  console.log(hamburger.innerHTML);
+  if (hamburger.classList.contains('close-button')) {
+    hamburger.innerHTML = '<span class="hamburger color-1">&#9776;</span>';
+    hamburger.classList.remove('close-button');
+  } else {
+    hamburger.classList.add('close-button');
+    hamburger.innerHTML = '<span class="hamburger color-1">&#10006;</span>';
+  }
+};
